1c34bd791ba9cc23da4107e870f7c6e521da40d1
jensh007 <j.huebel@sap.com>
2023-06-22 15:34:57 +0200

    fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 import os
+import other
 
 def main():
     print(f'{os.environ=}')
     print(f'my input variable "ocm_images": {os.getenv("ocm_images")}')
+    other.other()
 
 
 if __name__ == '__main__':
