58effa454eb858e4d120ba7bb92c55e6680ccdb4
jensh007 <j.huebel@sap.com>
2023-06-22 16:45:10 +0200

    fix

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python3
 import os
 import sys
+from ruamel.yaml import YAML
 
 import other
 
 def main():
-    print(f'my input variable "ocm_images": {os.getenv("ocm_images")}')
+    image_yaml = os.getenv("ocm_images")
+    print(f'my input variable "ocm_images": {image_yaml}')
     other.other()
     print(f' Python version: {sys.version_info}')
+
+    yaml=YAML(typ='safe')
+    d = yaml.load(image_yaml)
+    print(f'loaded yaml input: {d}')
 
 if __name__ == '__main__':
     main()
